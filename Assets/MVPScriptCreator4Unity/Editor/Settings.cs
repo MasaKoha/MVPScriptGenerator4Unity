723d105f62d210e182f0138240c238f167783f31
Masataka Kohagura <masalinkinpark@gmail.com>
2018-07-17 00:45:20 +0900

    Create Window for MVP Script Creator

---
+++ Assets/MVPScriptCreator4Unity/Editor/Settings.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace MVP4U.Editor
+{
+    public class Settings : ScriptableObject
+    {
+        public string Namespace;
+    }
+}
